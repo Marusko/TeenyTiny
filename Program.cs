095569374eaac95f20d7a06c05af0559163b543a
Matúš Suský <matussusky@gmail.com>
2024-06-06 17:56:16 +0200

    Edited main method to use parser

--- Program.cs
+++ Program.cs
@@ -1,19 +1,30 @@
-﻿namespace Teeny_Tiny
+﻿using System.Text;
+
+namespace Teeny_Tiny
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            var source = "IF+-123 foo*THEN/";
-            Lexer lexer = new Lexer(source);
+            Console.WriteLine("Teeny Tiny Compiler");
 
-            var token = lexer.GetToken();
-
-            while (token.Kind != TokenType.EOF)
+            if (args.Length != 2)
             {
-                Console.WriteLine($"{token.Kind} :: {token.Text}");
-                token = lexer.GetToken();
+                Console.WriteLine("Error: Compiler needs source file as argument.");
+                Environment.Exit(1);
             }
+
+            string source;
+            using (StreamReader reader = new StreamReader(args[1], Encoding.UTF8))
+            {
+                source = reader.ReadToEnd();
+            }
+
+            Lexer lexer = new Lexer(source);
+            Parser parser = new Parser(lexer);
+
+            parser.program();
+            Console.WriteLine("Parsing completed.");
         }
     }
 }
