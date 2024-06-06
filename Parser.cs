a05ce0a7a9012ad54c5868bd7d2ba548315a4c36
Matúš Suský <matussusky@gmail.com>
2024-06-06 17:55:59 +0200

    Added parser class skeleton

---
+++ Parser.cs
@@ -0,0 +1,27 @@
+﻿namespace Teeny_Tiny
+{
+    public class Parser
+    {
+        public Parser(Lexer lexer)
+        {
+        }
+
+        // Return true if the current token matches.
+        public bool CheckToken(TokenType kind){}
+
+        // Return true if the next token matches.
+        public bool CheckPeek(TokenType kind){}
+
+        // Try to match current token. If not, error. Advances the current token.
+        public void Match(TokenType kind){}
+
+        // Advances the current token.
+        public void NextToken(){}
+
+        public void Abort(string message)
+        {
+            Console.WriteLine($"Error. {message}");
+            Environment.Exit(1);
+        }
+    }
+}
