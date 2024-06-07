de71c54c774ebf19de6512dd16c2a2169010bcd8
Matúš Suský <matussusky@gmail.com>
2024-06-07 12:40:05 +0200

    Completed emitter

---
+++ Emitter.cs
@@ -0,0 +1,35 @@
+﻿namespace Teeny_Tiny
+{
+    public class Emitter
+    {
+        public string FullPath { get; set; }
+        public string Header { get; set; } = "";
+        public string Code { get; set; } = "";
+
+        public Emitter(string fullPath)
+        {
+            FullPath = fullPath;
+        }
+
+        public void Emit(string code)
+        {
+            Code += code;
+        }
+
+        public void EmitLine(string code)
+        {
+            Code += code + '\n';
+        }
+
+        public void HeaderLine(string code)
+        {
+            Header += code + '\n';
+        }
+
+        public void WriteFile()
+        {
+            using StreamWriter writer = new StreamWriter(FullPath);
+            writer.Write(Header + Code);
+        }
+    }
+}
